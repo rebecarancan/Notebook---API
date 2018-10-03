efd5a44a5289d3b974b84cc09b8c5889f05aa095
Rebeca Rancan <rebecarancanrr@gmail.com>
2018-10-03 15:18:18 -0300

    Versionamento por Subdomínio ou Hostname

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,27 @@ Rails.application.routes.draw do
   # resources :auths, only: [:create]
   resources :kinds
 
-    api_version(:module => "V1", :header => {:name => "Accept", :value => "application/vnd.api+json; version=1"}) do
-    #api_version(:module => "V1", :header => {:name => "X-Version", :value => "version=1"}) do  # Header Customizado
-      resources :contacts do
+    constraints subdomain: 'v1' do
+      scope module: 'v1' do
+        resources :contacts do
+          resource :kind, only: [:show]
+          resource :kind, only: [:show], path: 'relationships/kind'
+
+          resource :phones, only: [:show]
+          resource :phones, only: [:show], path: 'relationships/phones'
+
+          resource :phone, only: [:update, :create, :destroy]
+          resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
+
+          resource :address, only: [:show, :update, :create, :destroy]
+          resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
+        end
+      end
+    end
+
+    constraints subdomain: 'v2' do
+      scope module: 'v2' do
+        resources :contacts do
         resource :kind, only: [:show]
         resource :kind, only: [:show], path: 'relationships/kind'
 
@@ -19,21 +37,6 @@ Rails.application.routes.draw do
         resource :address, only: [:show, :update, :create, :destroy]
         resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
       end
-    end
-
-    api_version(:module => "V2", :header => {:name => "Accept", :value => "application/vnd.api+json; version=2"}) do
-      resources :contacts do
-      resource :kind, only: [:show]
-      resource :kind, only: [:show], path: 'relationships/kind'
-
-      resource :phones, only: [:show]
-      resource :phones, only: [:show], path: 'relationships/phones'
-
-      resource :phone, only: [:update, :create, :destroy]
-      resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
-
-      resource :address, only: [:show, :update, :create, :destroy]
-      resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
     end
   end
 
